ad48a0bb7005e5c5c4b13b9e45e43d03ef48f1dd
shubhamtunwal97 <shubhamtunwal97@gmail.com>
2018-12-27 13:35:03 +0530

    Add files via upload

---
+++ MessageEncryptor.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apitests;
+
+import java.io.IOException;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ *
+ * @author shubhamtunwal
+ */
+public class MessageEncryptor {
+
+
+ static char[] chars = {
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+        'y', 'z', '0', '1', '2', '3', '4', '5',
+        '6', '7', '8', '9', 'A', 'B', 'C', 'D',
+        'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+        'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@',
+        '#', '$', '%', '^', '&', '(', ')', '+',
+        '-', '*', '/', '[', ']', '{', '}', '=',
+        '<', '>', '?', '_', '"', '.', ',', ' '
+    };
+    
+    public static void main(String[] args) 
+    {
+        String text ="Your#Message#here";
+        int offset = 19;
+        
+        String enc = encrypt(text, offset);
+        System.out.println("Encrypted text: " + enc);
+        
+        String dec = decrypt(enc, offset);
+        System.out.println("Decrypted text: " + dec);
+    }
+
+    // Caesar cipher
+    static String encrypt(String text, int offset)
+    {
+        char[] plain = text.toCharArray();
+
+        for (int i = 0; i < plain.length; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                if (j <= chars.length - offset) {
+                    if (plain[i] == chars[j]) {
+                        plain[i] = chars[j + offset];
+                        break;
+                    }
+                } 
+                else if (plain[i] == chars[j]) {
+                    plain[i] = chars[j - (chars.length - offset + 1)];
+                }
+            }
+        }
+        return String.valueOf(plain);
+    }
+
+    static String decrypt(String cip, int offset)
+    {
+        char[] cipher = cip.toCharArray();
+        for (int i = 0; i < cipher.length; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                if (j >= offset && cipher[i] == chars[j]) {
+                    cipher[i] = chars[j - offset];
+                    break;
+                }
+                if (cipher[i] == chars[j] && j < offset) {
+                    cipher[i] = chars[(chars.length - offset +1) + j];
+                    break;
+                }
+            }
+        }
+        return String.valueOf(cipher);
+    }
+}
